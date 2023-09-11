3e9aa634e2e0ebbc1baaf2400754c701da7bab02
Matt Rollender <matt@rollender.com>
2023-09-11 18:55:44 -0400

    overlay fix

--- flake.nix
+++ flake.nix
@@ -6,30 +6,34 @@
   inputs.nix-npm-buildpackage.url = "github:serokell/nix-npm-buildpackage";
 
   outputs = { self, nixpkgs, flake-utils, nix-npm-buildpackage }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system}.extend
-          nix-npm-buildpackage.overlays.default;
-        nodejs = pkgs.nodejs-18_x;
-        hubspot-cli = pkgs.buildYarnPackage {
-          src = pkgs.fetchFromGitHub {
-            owner = "HubSpot";
-            repo = "hubspot-cli";
-            sparseCheckout = [ "packages/cli" ];
-            rev = "0f88273233124d513c4338fed7bd01fa4678251d";
-            sha256 = "sha256-Kt8KcEsP85plrE2RjbujXW0rpF7/4BJG3XiGHDWPVi0=";
+    let
+      outputs = flake-utils.lib.eachDefaultSystem (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system}.extend
+            nix-npm-buildpackage.overlays.default;
+          nodejs = pkgs.nodejs-18_x;
+          hubspot-cli = pkgs.buildYarnPackage {
+            src = pkgs.fetchFromGitHub {
+              owner = "HubSpot";
+              repo = "hubspot-cli";
+              sparseCheckout = [ "packages/cli" ];
+              rev = "0f88273233124d513c4338fed7bd01fa4678251d";
+              sha256 = "sha256-Kt8KcEsP85plrE2RjbujXW0rpF7/4BJG3XiGHDWPVi0=";
+            };
+            postInstall = ''
+                            makeWrapper $out/packages/cli/bin/hs $out/bin/hs 
+              	    '';
           };
-          postInstall = ''
-                          makeWrapper $out/packages/cli/bin/hs $out/bin/hs 
-            	    '';
-        };
-      in {
-        overlays.default = final: prev: { inherit hubspot-cli; };
-        packages.default = hubspot-cli;
-        app.default = {
-          type = "app";
-          program = "${hubspot-cli}/bin/hs";
-        };
-      });
+        in {
+          packages.default = hubspot-cli;
+          app.default = {
+            type = "app";
+            program = "${hubspot-cli}/bin/hs";
+          };
+        });
+    in outputs // {
+      overlays.default = final: prev: {
+        hubspot-cli = outputs.${prev.system}.packages.hubspot-cli;
+      };
+    };
 }
-
